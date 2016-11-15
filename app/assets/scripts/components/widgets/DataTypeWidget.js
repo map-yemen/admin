c98d8b15f301fbb7d4a954356a5f3ba8291362c8
alifelski <ali@developmentseed.org>
2016-11-15 17:33:15 -0500

    styled data examples section

--- app/assets/scripts/components/widgets/DataTypeWidget.js
+++ app/assets/scripts/components/widgets/DataTypeWidget.js
@@ -24,46 +24,19 @@ export default class DataTypeWidget extends React.Component {
           <option value="Diverging">Diverging</option>
           <option value="Categorical">Categorical</option>
         </select>
-        <br />
-        <div>
-          <label>Sequential</label>
-          <p>Data is an increasing or decreasing range</p>
-          <div className="ramp BuGn">
-            <svg width="75" height="15">
-              <rect fill="rgb(237,248,251)" width="15" height="15" x="0"></rect>
-              <rect fill="rgb(178,226,226)" width="15" height="15" x="15"></rect>
-              <rect fill="rgb(102,194,164)" width="15" height="15" x="30"></rect>
-              <rect fill="rgb(44,162,95)" width="15" height="15" x="45"></rect>
-              <rect fill="rgb(0,109,44)" width="15" height="15" x="60"></rect>
-            </svg>
+        <div className="field-example">
+          <h3 className="header-small">Type of Data Examples</h3>
+          <div className="field-example-item">
+            <h4 className="header-smallest">Sequential</h4>
+            <p>Data is an increasing or decreasing range</p>
           </div>
-        </div>
-        <br />
-        <div>
-          <label>Diverging</label>
-          <p>Data highlights two opposing extremes</p>
-          <div className="ramp RdBu">
-            <svg width="75" height="15">
-              <rect fill="rgb(202,0,32)" width="15" height="15" x="0"></rect>
-              <rect fill="rgb(244,165,130)" width="15" height="15" x="15"></rect>
-              <rect fill="rgb(247,247,247)" width="15" height="15" x="30"></rect>
-              <rect fill="rgb(146,197,222)" width="15" height="15" x="45"></rect>
-              <rect fill="rgb(5,113,176)" width="15" height="15" x="60"></rect>
-            </svg>
+          <div className="field-example-item">
+            <h4 className="header-smallest">Diverging</h4>
+            <p>Data highlights two opposing extremes</p>
           </div>
-        </div>
-        <br />
-        <div>
-          <label>Categorical</label>
-          <p>Data represents different categories</p>
-          <div className="ramp Accent">
-            <svg width="75" height="15">
-              <rect fill="rgb(127,201,127)" width="15" height="15" x="0"></rect>
-              <rect fill="rgb(190,174,212)" width="15" height="15" x="15"></rect>
-              <rect fill="rgb(253,192,134)" width="15" height="15" x="30"></rect>
-              <rect fill="rgb(255,255,153)" width="15" height="15" x="45"></rect>
-              <rect fill="rgb(56,108,176)" width="15" height="15" x="60"></rect>
-            </svg>
+          <div className="field-example-item">
+            <h4 className="header-smallest">Categorical</h4>
+            <p>Data represents different categories</p>
           </div>
         </div>
       </div>
