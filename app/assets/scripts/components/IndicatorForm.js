10b398ec517564f56e2820d6dd779f9de1d9877d
Drew Bollinger <drew@developmentseed.org>
2017-01-17 15:06:46 -0500

    Match indicator type selections with project options

--- app/assets/scripts/components/IndicatorForm.js
+++ app/assets/scripts/components/IndicatorForm.js
@@ -28,30 +28,31 @@ export const schema = {
       title: 'Type of Data - نمط البيانات',
       type: 'string'
     },
-    theme: {
-      title: 'Theme - موضوع',
-      type: 'object',
-      properties: {
-        ar: {type: 'string'},
-        en: {type: 'string'}
+    sds_indicator: {
+      title: 'SDS Goals - أهداف استراتيجية التنمية المُستدامة',
+      type: 'array',
+      items: {
+        title: 'SDS Goal - هدف استراتيجية التنمية المُستدامة',
+        type: 'object',
+        properties: {en: {type: 'string'}, ar: {type: 'string'}}
       }
     },
-    indicator_type: {
-      title: 'Type of Indicator - نوع المؤشر',
-      type: 'object',
-      properties: {
-        sds: {
-          title: 'SDS Indicator - مؤشرات استراتيجية التنمية المُستدامة',
-          type: 'boolean'
-        },
-        sdg: {
-          title: 'SDG Indicator - مؤشرات أهداف التنمية المُستدامة',
-          type: 'boolean'
-        },
-        other: {
-          title: 'Other Development Indicator - مؤشرات إنمائية أخرى',
-          type: 'boolean'
-        }
+    sdg_indicator: {
+      title: 'SDG Goals - أهداف التنمية المستدامة',
+      type: 'array',
+      items: {
+        title: 'SDG Goal - هدف التنمية المستدامة',
+        type: 'object',
+        properties: {en: {type: 'string'}, ar: {type: 'string'}}
+      }
+    },
+    themer: {
+      type: 'array',
+      title: 'Theme - موضوع',
+      items: {
+        title: 'Theme - موضوع',
+        type: 'object',
+        properties: {en: {type: 'string'}, ar: {type: 'string'}}
       }
     },
     sources: {
@@ -91,11 +92,26 @@ const uiSchema = {
     classNames: 'ar',
     'ui:widget': 'textarea'
   },
-  theme: {
-    'ui:field': 'select-theme'
+  themer: {
+    classNames: 'multiform-group',
+    items: {
+      classNames: 'multiform-group_item',
+      'ui:field': 'select-themer'
+    }
   },
-  indicator_type: {
-    classNames: 'indicator-checkbox'
+  sds_indicator: {
+    classNames: 'multiform-group',
+    items: {
+      classNames: 'multiform-group_item',
+      'ui:field': 'select-sds_indicator'
+    }
+  },
+  sdg_indicator: {
+    classNames: 'multiform-group',
+    items: {
+      classNames: 'multiform-group_item',
+      'ui:field': 'select-sdg_indicator'
+    }
   },
   category: {
     'ui:field': 'datatype'
@@ -144,7 +160,81 @@ class IndicatorForm extends React.Component {
       showErrorList={false}
       fields={{
         'datatype': DataTypeWidget,
-        'select-theme': Dropdown('Theme - موضوع', 'Select a Theme',
+        'select-sds_indicator': Dropdown(
+          'SDS Goal - هدف استراتيجية التنمية المُستدامة',
+          'Select an SDS goal - يُرجى اختيار أحد أهداف استراتيجية التنمية المستدامة التى يتناولها المشروع',
+          [
+            'Pillar 1: Economic Development',
+            'Pillar 2: Energy',
+            'Pillar 3: Knowledge, Innovation and Scientific Research',
+            'Pillar 4: Transparency and Efficiency of Government Institutions',
+            'Pillar 5: Social Justice',
+            'Pillar 6: Health',
+            'Pillar 7: Education & Training',
+            'Pillar 8: Culture',
+            'Pillar 9: Environment',
+            'Pillar 10: Urban Development',
+            'Pillar 11: National Security and Foreign Policy',
+            'Pillar 12: Domestic Policy'
+          ],
+          [
+            'المحور الأول: التنمية الاقتصادية',
+            'المحور الثاني: الطاقة',
+            'المحور الثالث: المعرفة والابتكار والبحث العلمي',
+            'المحور الرابع: شفافية وكفاءة المؤسسات الحكومية',
+            'المحور الخامس: العدالة الاجتماعية',
+            'المحور السادس: الصحة',
+            'المحور السابع: التعليم والتدريب',
+            'المحور الثامن: الثقافة',
+            'المحور التاسع: البيئة',
+            'المحور العاشر: التنمية العمرانية',
+            '',
+            ''
+          ],
+        ),
+        'select-sdg_indicator': Dropdown(
+          'SDG Goal - هدف التنمية المستدامة',
+          'Select an SDG goal - يُرجى اختيار أحد أهداف التنمية المُستدامة التى يتناولها المشروع',
+          [
+            'Goal 1: End poverty in all its forms everywhere',
+            'Goal 2: End hunger, achieve food security and improved nutrition and promote sustainable agriculture',
+            'Goal 3: Ensure healthy lies and promote well being for all at all ages',
+            'Goal 4: Ensure inclusive and equitable education and promote lifelong learning opportunities for all',
+            'Goal 5: Achieve gender equality and empower all women and girls',
+            'Goal 6: Ensure availability and sustainable management of water and sanitation for all',
+            'Goal 7: Ensure access to affordable, reliable, sustainable, and modern energy for all',
+            'Goal 8: Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all',
+            'Goal 9: Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation',
+            'Goal 10: Reduce inequality within and among countries',
+            'Goal 11: Make cities and human settlements inclusive, safe, resilient, and sustainable',
+            'Goal 12: Ensure sustainable consumption and production patterns',
+            'Goal 13: Take urgent action to combat climate change and its impacts',
+            'Goal 14: Conserve and sustainably use the oceans and marine resources for sustainable development',
+            'Goal 15: Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification and halt and reverse land degradation and halt biodiversity loss',
+            'Goal 16: Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable, and inclusive institutions at all levels',
+            'Goal 17: Strengthen the means of implementation and revitalize the global partnership for sustainable development'
+          ],
+          [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            ''
+          ],
+        ),
+        'select-themer': Dropdown('Theme - موضوع', 'Select a Theme',
           [
             'Education & Training',
             'Health',
