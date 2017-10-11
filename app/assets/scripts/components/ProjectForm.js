d3af8cdaa3f004cdf45c83bdf58a5abc19f8f4d5
Marc Farra <marcfarra@gmail.com>
2017-10-11 17:19:35 -0400

    Add more translations

--- app/assets/scripts/components/ProjectForm.js
+++ app/assets/scripts/components/ProjectForm.js
@@ -8,6 +8,7 @@ import CustomTextWidget from './widgets/CustomTextWidget';
 import CustomNumberWidget from './widgets/CustomNumberWidget';
 import Dropdown from './widgets/Dropdown';
 import {setMaybe, transformErrors} from '../utils/nullUtils';
+import {sdsLabels, sdgLabels} from '../utils/labels';
 
 export const schema = {
   type: 'object',
@@ -633,78 +634,16 @@ class ProjectForm extends React.Component {
         ),
         'select-sds_indicator': Dropdown(
           'SDS Goal - هدف استراتيجية التنمية المُستدامة',
-          'Select an SDS goal - يُرجى اختيار أحد أهداف استراتيجية التنمية المستدامة التى يتناولها المشروع',
-          [
-            'Pillar 1: Economic Development',
-            'Pillar 2: Energy',
-            'Pillar 3: Knowledge, Innovation and Scientific Research',
-            'Pillar 4: Transparency and Efficiency of Government Institutions',
-            'Pillar 5: Social Justice',
-            'Pillar 6: Health',
-            'Pillar 7: Education & Training',
-            'Pillar 8: Culture',
-            'Pillar 9: Environment',
-            'Pillar 10: Urban Development',
-            'Pillar 11: National Security and Foreign Policy',
-            'Pillar 12: Domestic Policy'
-          ],
-          [
-            'المحور الأول: التنمية الاقتصادية',
-            'المحور الثاني: الطاقة',
-            'المحور الثالث: المعرفة والابتكار والبحث العلمي',
-            'المحور الرابع: شفافية وكفاءة المؤسسات الحكومية',
-            'المحور الخامس: العدالة الاجتماعية',
-            'المحور السادس: الصحة',
-            'المحور السابع: التعليم والتدريب',
-            'المحور الثامن: الثقافة',
-            'المحور التاسع: البيئة',
-            'المحور العاشر: التنمية العمرانية',
-            '',
-            ''
-          ],
+          sdsLabels.select,
+          sdsLabels.en,
+          sdsLabels.ar,
           true
         ),
         'select-sdg_indicator': Dropdown(
           'SDG Goal - هدف التنمية المستدامة',
-          'Select an SDG goal - يُرجى اختيار أحد أهداف التنمية المُستدامة التى يتناولها المشروع',
-          [
-            'Goal 1: End poverty in all its forms everywhere',
-            'Goal 2: End hunger, achieve food security and improved nutrition and promote sustainable agriculture',
-            'Goal 3: Ensure healthy lies and promote well being for all at all ages',
-            'Goal 4: Ensure inclusive and equitable education and promote lifelong learning opportunities for all',
-            'Goal 5: Achieve gender equality and empower all women and girls',
-            'Goal 6: Ensure availability and sustainable management of water and sanitation for all',
-            'Goal 7: Ensure access to affordable, reliable, sustainable, and modern energy for all',
-            'Goal 8: Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all',
-            'Goal 9: Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation',
-            'Goal 10: Reduce inequality within and among countries',
-            'Goal 11: Make cities and human settlements inclusive, safe, resilient, and sustainable',
-            'Goal 12: Ensure sustainable consumption and production patterns',
-            'Goal 13: Take urgent action to combat climate change and its impacts',
-            'Goal 14: Conserve and sustainably use the oceans and marine resources for sustainable development',
-            'Goal 15: Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification and halt and reverse land degradation and halt biodiversity loss',
-            'Goal 16: Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable, and inclusive institutions at all levels',
-            'Goal 17: Strengthen the means of implementation and revitalize the global partnership for sustainable development'
-          ],
-          [
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            ''
-          ],
+          sdgLabels.select,
+          sdgLabels.en,
+          sdgLabels.ar,
           true
         ),
         'select-category': Dropdown(
